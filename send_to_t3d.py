425a4cd35c31ab0cdb9eaf80f4ba92acd86e6b93
leeroyjenkins-87 <68830045+leeroyjenkins-87@users.noreply.github.com>
2022-01-08 00:02:00 -0500

    Added py files and Condensed Code

--- send_to_t3d.py
+++ send_to_t3d.py
@@ -1,4 +1,6 @@
-
+import bpy
+import bmesh
+import pyperclip
 
 
 class sendToT3d(bpy.types.Operator):
@@ -6,100 +8,86 @@ class sendToT3d(bpy.types.Operator):
     bl_label = "Send to T3d"
     bl_description = "Creates T3d File for UDK"
 
-
+    
     @classmethod
     def poll(cls, context):
         return context.selected_objects is not None
-
+    
     def execute(self, context):
-
+        
         objectString = bpy.context.active_object
-
+        
         objectStringName = objectString.name.rstrip('.0123456789')
-
-        textT3d = 'Begin PolyList\n'
-
+        
+        bpy.context.scene.textT3d = 'Begin PolyList'
+        
+        linkCount = 0
+        
         for f in objectString.data.polygons:
-
-            polyNormal = f.normal
-
-            if ('-' in str(polyNormal.x)):
-                polyX = str(polyNormal.x).lstrip('-').split('.')
-                polyXString = '-' + polyX[0].zfill(5) + '.' + polyX[1].ljust(6, '0')
-            else:
-                polyX = str(polyNormal.x).split('.')
-                polyXString = '+' + polyX[0].zfill(5) + '.' + polyX[1].ljust(6, '0')
-
-            if ('-' in str(polyNormal.y)):
-                polyY = str(polyNormal.y).lstrip('-').split('.')
-                polyYString = '-' + polyY[0].zfill(5) + '.' + polyY[1].ljust(6, '0')
-            else:
-                polyY = str(polyNormal.y).split('.')
-                polyYString = '+' + polyY[0].zfill(5) + '.' + polyY[1].ljust(6, '0')
-
-            if ('-' in str(polyNormal.z)):
-                polyZ = str(polyNormal.z).lstrip('-').split('.')
-                polyZString = '-' + polyZ[0].zfill(5) + '.' + polyZ[1].ljust(6, '0')
-            else:
-                polyZ = str(polyNormal.z).split('.')
-                polyZString = '+' + polyZ[0].zfill(5) + '.' + polyZ[1].ljust(6, '0')
-
-            polyString = '\t\tNormal   {},{},{}'.format(polyXString, polyYString, polyZString)
-
+            
+            stringList = [linkCount]
+            
+            originShort = objectString.data.vertices[f.vertices[0]].co
+            
+            originList = [originShort.x, originShort.y, originShort.z]
+            
+            stringList.extend(self.stringFormatter(originList, 100))
+            
+            polyList = [f.normal.x, f.normal.y, f.normal.z]
+            
+            stringList.extend(self.stringFormatter(polyList, 1))
+            
             if f.normal.x != 0:
-                uString = '\t\tTextureU +00000.000000,+00001.000000,+00000.000000'
-                vString = '\t\tTextureV +00000.000000,+00000.000000,+00001.000000'
+                stringList.extend(['+00000.000000','+00001.000000','+00000.000000','+00000.000000','+00000.000000','+00001.000000'])
             elif f.normal.y !=0:
-                uString = '\t\tTextureU +00001.000000,+00000.000000,+00000.000000'
-                vString = '\t\tTextureV +00000.000000,+00000.000000,+00001.000000'
+                stringList.extend(['+00001.000000','+00000.000000','+00000.000000','+00000.000000','+00000.000000','+00001.000000'])
             else:
-                uString = '\t\tTextureU +00001.000000,+00000.000000,+00000.000000'
-                vString = '\t\tTextureV +00000.000000,+00001.000000,+00000.000000'
-
-            verticeString = ''
-
+                stringList.extend(['+00001.000000','+00000.000000','+00000.000000','+00000.000000','+00001.000000','+00000.000000'])
+            
             for idx in f.vertices:
-                if ('-' in str(objectString.data.vertices[idx].co.x)):
-                    idxX = str(objectString.data.vertices[idx].co.x * 100).lstrip('-').split('.')
-                    idxXString = '-' + idxX[0].zfill(5) + '.' + idxX[1].ljust(6, '0')
-                else:
-                    idxX = str(objectString.data.vertices[idx].co.x * 100).split('.')
-                    idxXString = '+' + idxX[0].zfill(5) + '.' + idxX[1].ljust(6, '0')
 
-                if ('-' in str(objectString.data.vertices[idx].co.y)):
-                    idxY = str(objectString.data.vertices[idx].co.y * 100).lstrip('-').split('.')
-                    idxYString = '-' + idxY[0].zfill(5) + '.' + idxY[1].ljust(6, '0')
-                else:
-                    idxY = str(objectString.data.vertices[idx].co.y * 100).split('.')
-                    idxYString = '+' + idxY[0].zfill(5) + '.' + idxY[1].ljust(6, '0')
+                idxList = [objectString.data.vertices[idx].co.x, objectString.data.vertices[idx].co.y, objectString.data.vertices[idx].co.z]
 
-                if ('-' in str(objectString.data.vertices[idx].co.z)):
-                    idxZ = str(objectString.data.vertices[idx].co.z * 100).lstrip('-').split('.')
-                    idxZString = '-' + idxZ[0].zfill(5) + '.' + idxZ[1].ljust(6, '0')
-                else:
-                    idxZ = str(objectString.data.vertices[idx].co.z * 100).split('.')
-                    idxZString = '+' + idxZ[0].zfill(5) + '.' + idxZ[1].ljust(6, '0')
+                stringList.extend(self.stringFormatter(idxList, 100))
 
-                verticeString += '\t\tVertex   {},{},{}\n'.format(idxXString, idxYString, idxZString)
+            linkCount += 1
+            
+            bpy.context.scene.textT3d += """\n\tBegin Polygon Flags=3584 Link={}
+        Origin   {},{},{}
+        Normal   {},{},{}
+        TextureU {},{},{}
+        TextureV {},{},{}
+        Vertex   {},{},{}
+        Vertex   {},{},{}
+        Vertex   {},{},{}
+        Vertex   {},{},{}
+    End Polygon""".format(*stringList)
 
-
-            polyOrigin = verticeString.split('\n')
-
-            polyOriginString = polyOrigin[0].replace('Vertex', 'Origin')
-
-            startString = '\tBegin Polygon Texture=EngineMaterials.DefaultMaterial Flags=3584'
-
-            endString = '\tEnd Polygon'
-
-            textT3d += '{}\n{}\n{}\n{}\n{}\n{}{}\n'.format(startString, polyOriginString, polyString, uString, vString, verticeString, endString)
-
-        textT3d += 'End PolyList'
+        bpy.context.scene.textT3d += '\nEnd PolyList'
+        print(bpy.context.scene.textT3d)
         fileName = '{}{}'.format(objectStringName, '.t3d')
         outputT3d = '{}{}'.format(bpy.path.abspath(bpy.context.scene.conf_path), fileName)
-
+       
         f = open( outputT3d, 'w' )
-        f.writelines( textT3d.rstrip() )
-        f.close()
-        pyperclip.copy(textT3d.rstrip())
-
+        f.writelines( bpy.context.scene.textT3d.rstrip() )
+        f.close()   
+        pyperclip.copy(bpy.context.scene.textT3d.rstrip())
+        
         return {'FINISHED'}
+    
+    def stringFormatter(self, inVar, multiple):
+        
+        outVar = []
+        
+        for var in inVar:
+            
+            if ('-' in str(var)):
+                idxVar = str(round(var * multiple, 6)).lstrip('-').split('.')
+                idxVarString = '-' + idxVar[0].zfill(5) + '.' + idxVar[1].ljust(6, '0')
+            else:
+                idxVar = str(round(var * multiple, 6)).split('.')
+                idxVarString = '+' + idxVar[0].zfill(5) + '.' + idxVar[1].ljust(6, '0')
+
+            outVar.append(idxVarString)
+            
+        return outVar
--- send_to_udk.py
+++ send_to_udk.py
@@ -1,132 +1,183 @@
-
-
+import bpy
+# Don't think i need bmesh
+#import bmesh 
+import pyperclip
+# Don't think i need Mathutils or pi
+#from mathutils import Matrix, Vector
+# Don't think i need radians or pi
+from math import degrees#, radians, pi
+from def_obj_vars import *
+from cust_obj_vars import *
 
 class sendToUDK(bpy.types.Operator):
     bl_idname = "custom.send_to_udk"
     bl_label = "Send to UDK"
     bl_description = "Creates Object Data"
 
-
+    
     @classmethod
     def poll(cls, context):
         return context.selected_objects is not None
-
+    
     def execute(self, context):
-
+        
         outputFile = '{}{}'.format(bpy.path.abspath(bpy.context.scene.conf_path), "Blender2UDK.csv")
         textUDK = ""
-
-        if bpy.context.scene.collectData == True:
-            objectString = bpy.data.collections[bpy.context.scene.collectionHolder].objects[:]
+        
+        if bpy.context.scene.collectRotations == True:
+            objCount = len(bpy.data.collections[bpy.context.scene.collectionHolder].objects[:])
         else:
-            objectString = bpy.context.selected_objects
-
-        for obj in objectString:
-            if obj.type != 'MESH':
-                continue
-
+            objHolder = bpy.context.selected_objects
+            objCount = len(bpy.context.selected_objects[:])
+        
+        for selectedObj in bpy.context.selected_objects:
+            selectedObj.select_set(False)
+            
+        loopCount = 0
+        
+        while loopCount < objCount:
+            
+            if bpy.context.scene.collectRotations == True: 
+                obj = bpy.data.collections[bpy.context.scene.collectionHolder].objects[loopCount]
+            else:
+                obj = objHolder[loopCount]
+        
+            obj.select_set(True)
+            
+            bpy.context.view_layer.objects.active = obj
+            
             # LOCATION XYZ
             # --------------------------------------------------------------------
-            center = obj.location
-            centerX, centerY, centerZ = center[0]*100, center[1]*100, center[2]*100
-            d, e, f = round(-centerX, 6), round(centerY, 6), round(centerZ, 6),
-            locationString = 'Location=(X={},Y={},Z={})'.format(d, e, f)
-
-            # ROTATION XYZ
+            locCount = 0
+            locList = []
+            
+            for idxLoc in obj.location:
+                if locCount == 0:
+                    idxLoc = -idxLoc
+                locList.append(round(idxLoc * 100, 6))
+                locCount += 1
+            
+            # ROTATION XYZ 
             # --------------------------------------------------------------------
-            orientationX = degrees(obj.rotation_euler.x)*-1
-            orientationY = degrees(obj.rotation_euler.y)
-            orientationZ = degrees(obj.rotation_euler.z)*-1
-            a, b, c = round(orientationX * (65536 / 360)), round(orientationY * (65536 / 360)), round(orientationZ * (65536 / 360))
-            rotationString = 'Rotation=(Pitch={},Yaw={},Roll={})'.format(b, c, a)
+            rotList = self.stringFormatter(obj.rotation_euler)
 
             # SCALE XYZ
             # --------------------------------------------------------------------
-            objScaleX = round(obj.scale[0], 6)
-            objScaleY = round(obj.scale[1], 6)
-            objScaleZ = round(obj.scale[2], 6)
-            scaleString = 'DrawScale3D=(X={},Y={},Z={})'.format(objScaleX, objScaleY, objScaleZ)
+            scaleList = []
+            
+            for objScale in obj.scale:
+                scaleList.append(round(objScale, 6))
 
             # PROJECT NAME
             # --------------------------------------------------------------------
             mapDirectory = bpy.context.scene['projectName'].split('.')
             mapName = mapDirectory[-2].split("\\")
-
+            
             # STATICMESH NAME
             # --------------------------------------------------------------------
             objName = obj.name.split('.')
             staticString = "{}.{}".format(mapName[-1], objName[0])
-
+            
+            
             # MATERIAL LIST
             # --------------------------------------------------------------------
             materialCount = 0
             materialNames = ''
-
+            
             if bpy.context.scene.collectMaterials == True:
-
+                
                 for objSlots in obj.material_slots:
                     materialSlots = objSlots.material.name.lstrip('"').rstrip('"').replace("'", "").lstrip("Material'")
                     materialString = "Materials({})=Material'{}'\n\t\t\t\t\t".format(materialCount, materialSlots)
                     materialCount += 1
                     materialNames += materialString
-
+            
             # LAYER
             # --------------------------------------------------------------------
-            layerString = 'Layer="{}"'.format(str(obj.users_collection[0].name.rstrip(".0123456789")))
-
+            
+            #looks like it should be str(obj.users_collection[0].name).rstrip(".0123456789")
+            layerString = str(obj.users_collection[0].name.rstrip(".0123456789"))
+            
             # TAG
             # --------------------------------------------------------------------
-            tagString = 'Tag="{}"'.format(str(objName[0]).rstrip(".0123456789"))
-
+            tagString = str(objName[0]).rstrip(".0123456789")
+            
             # PHYSICAL MATERIAL APPLIED
             # --------------------------------------------------------------------
             if bpy.context.scene.physMat == True:
-                physString = "PhysMaterialOverride=PhysicalMaterial'{}.Materials.StickyWalls'".format(mapName[-1].lower())
+                physString = 'PhysicalMaterials.Collision_Sticky'
             else:
                 physString = ''
+                
+            # FORMATTING FOR STATICMESH 
+            # --------------------------------------------------------------------
+            if ('StaticMesh' in str(objName)):
+                num = str(bpy.context.scene.numberSequencer).zfill(10)
+                
+                textUDK += staticMeshString.format(num, staticString, materialNames.rstrip(), physString, locList, rotList, scaleList, tagString, layerString, objName)
+            
+            # FORMATTING FOR SPOTLIGHT 
+            # --------------------------------------------------------------------
+            elif ('Spot' in str(objName)):
+            
+                # ROTATION XYZ FOR SPOTLIGHTS IS NEGATIVE 90 DEGREES IN UDK SO WE HAVE TO ADJUST IT.
+                bpy.ops.custom.set_pos_y()
 
-            textUDK += """
-        Begin Map
-           Begin Level
-              Begin Actor Class=StaticMeshActor Name=StaticMeshActor_4 Archetype=StaticMeshActor'Engine.Default__StaticMeshActor'
-                 Begin Object Class=StaticMeshComponent Name=StaticMeshComponent0 ObjName=StaticMeshComponent Archetype=StaticMeshComponent'Engine.Default__StaticMeshActor:StaticMeshComponent0'
-                    StaticMesh=StaticMesh'{}'
-                    VertexPositionVersionNumber=1
-                    {}
-                    ReplacementPrimitive=None
-                    bAllowApproximateOcclusion=True
-                    bAcceptsDynamicDecals=False
-                    bForceDirectLightMap=True
-                    bUsePrecomputedShadows=True
-                    bDisableAllRigidBody=False
-                    LightingChannels=(bInitialized=True,Static=True)
-                    {}
-                    Name="StaticMeshComponent"
-                    ObjectArchetype=StaticMeshComponent'Engine.Default__StaticMeshActor:StaticMeshComponent0'
-                 End Object
-                 StaticMeshComponent=StaticMeshComponent'StaticMeshComponent0'
-                 Components(0)=StaticMeshComponent'StaticMeshComponent0'
-                 {}
-                 {}
-                 {}
-                 {}
-                 {}
-                 BlockRigidBody=True
-                 CreationTime=0
-                 Tag="StaticMeshActor"
-                 CollisionComponent=StaticMeshComponent'StaticMeshComponent'
-                 Name="StaticMeshActor"
-                 ObjectArchetype=StaticMeshActor'Engine.Default__StaticMeshActor'
-              End Actor
-           End Level
-        Begin Surface
-        End Surface
-        End Map
-        """.format(staticString, materialNames.rstrip(), physString, locationString, rotationString, scaleString, tagString, layerString)
+                rotList = self.stringFormatter(rot)
+                
+                # ADUJUST ROTATION BACK 90 DEGREES
+                bpy.ops.custom.set_neg_y()
+                
+                textUDK += spotLightString.format(num, locList, rotList, scaleList, tagString, layerString, objName)
+                
+            # FORMATTING FOR CUSTOM GOAL 
+            # --------------------------------------------------------------------
+            elif ('GoalVolume' in str(objName)):
+                teamNum = 0
+                
+                bpy.ops.custom.send_to_t3d()
+                
+                num = str(bpy.context.scene.numberSequencer).zfill(10)
+                
+                #IF LOCATION == NEGATIVE Y SET TO TEAM NUMBER 1
+                if locList[1] < 0:
+                    teamNum = 1
+
+                textUDK += goalString.format(num, teamNum, bpy.context.scene.textT3d, locList, rotList, scaleList, layerString)
+                
+            elif ('DynamicTrigger' in str(objName)):
+                    
+                bpy.ops.custom.send_to_t3d()
+                
+                num = str(bpy.context.scene.numberSequencer).zfill(10)
+                
+                textUDK += dynamicTriggerString.format(num, bpy.context.scene.textT3d, locList, rotList, scaleList, tagString, layerString, objName)
+
+            # FORMATTTING FOR EVERYTHING ELSE 
+            # --------------------------------------------------------------------
+            else:
+                textUDK += staticMeshString.format(num, staticString, materialNames.rstrip(), physString, locList, rotList, scaleList, tagString, layerString, objName)
+        
+            loopCount += 1
+            
+            bpy.context.scene.numberSequencer += 1
 
         f = open( outputFile, 'w' )
         f.writelines( textUDK.rstrip() )
-        f.close()
+        f.close()   
         pyperclip.copy(textUDK.rstrip())
-
+        
         return {'FINISHED'}
+        
+    def getRot(self, inVar):
+        tempList = []
+        flipRot = [-1,1,-1]
+        count = 0
+        
+        for idxRot in inVar:
+            multRot = round((degrees(idxRot) * flipRot[count]) * (65536 / 360))
+            tempList.append(multRot)
+            count += 1
+
+        outVar = [tempList[1],tempList[2],tempList[0]]
+        return outVar
