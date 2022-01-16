9fc347047cde463b7d4635975decd00203a20655
leeroyjenkins-87 <68830045+leeroyjenkins-87@users.noreply.github.com>
2022-01-15 23:27:15 -0500

    Had a bad if else statement and indentation.

--- send_to_udk.py
+++ send_to_udk.py
@@ -21,6 +21,7 @@ import pyperclip
 from math import degrees#, radians, pi
 from .def_obj_vars import *
 from .cust_obj_vars import *
+from mathutils import Matrix
 
 class sendToUDK(bpy.types.Operator):
     bl_idname = "custom.send_to_udk"
@@ -37,21 +38,25 @@ class sendToUDK(bpy.types.Operator):
         objCount = 0
         objHolder = []
         
-        if bpy.context.scene.collectData == True:
-            if bpy.context.scene.collectionHolder != '':
-                objCount = len(bpy.data.collections[bpy.context.scene.collectionHolder].objects[:])
-                self.runSend(objCount, objHolder)
-            else:
-                bpy.context.scene.errorCode = 2
-                bpy.ops.custom.error_message('INVOKE_DEFAULT')                
+        if bpy.context.scene.projectName == '':
+            bpy.context.scene.errorCode = 3
+            bpy.ops.custom.error_message('INVOKE_DEFAULT')
         else:
-            if len(bpy.context.selected_objects) < 1:
-                bpy.context.scene.errorCode = 1
-                bpy.ops.custom.error_message('INVOKE_DEFAULT')
+            if bpy.context.scene.collectData == True:
+                if bpy.context.scene.collectionHolder != '':
+                    objCount = len(bpy.data.collections[bpy.context.scene.collectionHolder].objects[:])
+                    self.runSend(objCount, objHolder)
+                else:
+                    bpy.context.scene.errorCode = 2
+                    bpy.ops.custom.error_message('INVOKE_DEFAULT')                
             else:
-                objHolder = bpy.context.selected_objects
-                objCount = len(bpy.context.selected_objects[:])
-                self.runSend(objCount, objHolder)
+                if len(bpy.context.selected_objects) < 1:
+                    bpy.context.scene.errorCode = 1
+                    bpy.ops.custom.error_message('INVOKE_DEFAULT')
+                else:
+                    objHolder = bpy.context.selected_objects
+                    objCount = len(bpy.context.selected_objects[:])
+                    self.runSend(objCount, objHolder)
             
         return{'FINISHED'}
         
@@ -62,13 +67,17 @@ class sendToUDK(bpy.types.Operator):
         objCount = inVar1
         objHolder = inVar2
         
-        num = str(bpy.context.scene.numberSequencer).zfill(10)
         outputFile = '{}{}'.format(bpy.path.abspath(bpy.context.scene.conf_path), "Blender2UDK.csv")
+        
         textUDK = ""
-            
+           
         loopCount = 0
         
+        archetype = defStaticArch
+        
         while loopCount < objCount:
+        
+            num = str(bpy.context.scene.numberSequencer).zfill(10)
             
             if bpy.context.scene.collectData == True: 
                 obj = bpy.data.collections[bpy.context.scene.collectionHolder].objects[loopCount]
@@ -79,16 +88,10 @@ class sendToUDK(bpy.types.Operator):
             
             bpy.context.view_layer.objects.active = obj
             
+            
             # LOCATION XYZ
             # --------------------------------------------------------------------
-            locCount = 0
-            locList = []
-            
-            for idxLoc in obj.location:
-                if locCount == 0:
-                    idxLoc = -idxLoc
-                locList.append(round(idxLoc * 100, 6))
-                locCount += 1
+            locList = self.getLoc(obj.location)
             
             # ROTATION XYZ 
             # --------------------------------------------------------------------
@@ -103,103 +106,158 @@ class sendToUDK(bpy.types.Operator):
 
             # PROJECT NAME
             # --------------------------------------------------------------------
+            mapDirectory = bpy.context.scene.projectName.split('.')
+            mapName = mapDirectory[-2].split("\\")
             
-            if bpy.context.scene.projectName == '':
-                bpy.context.scene.errorCode = 3
-                bpy.ops.custom.error_message('INVOKE_DEFAULT')
-
+            # STATICMESH NAME
+            # --------------------------------------------------------------------
+            objName = obj.name.split('.')
+            staticString = "{}.{}".format(mapName[-1], objName[0])
+            
+            # ArcheType
+            if bpy.context.scene.isArchetype == True:
+                archetype = staticString
+            
+            # MATERIAL LIST
+            # --------------------------------------------------------------------
+            materialCount = 0
+            materialNames = ''
+            
+            if bpy.context.scene.collectMaterials == True:
+                
+                for objSlots in obj.material_slots:
+                    materialSlots = objSlots.material.name.lstrip('"').rstrip('"').replace("'", "").lstrip("Material'")
+                    materialString = "Materials({})=Material'{}'\n\t\t\t\t\t".format(materialCount, materialSlots)
+                    materialCount += 1
+                    materialNames += materialString
+            
+            # LAYER
+            # --------------------------------------------------------------------
+            layerString = str(obj.users_collection[0].name.rstrip(".0123456789"))
+            
+            # TAG
+            # --------------------------------------------------------------------
+            tagString = str(objName[0]).rstrip(".0123456789")
+            
+            # PHYSICAL MATERIAL APPLIED
+            # --------------------------------------------------------------------
+            if bpy.context.scene.physMat == True:
+                physString = 'PhysicalMaterials.Collision_Sticky'
             else:
-                mapDirectory = bpy.context.scene.projectName.split('.')
-                mapName = mapDirectory[-2].split("\\")
+                physString = ''
                 
-                # STATICMESH NAME
-                # --------------------------------------------------------------------
-                objName = obj.name.split('.')
-                staticString = "{}.{}".format(mapName[-1], objName[0])
-                
-                
-                # MATERIAL LIST
-                # --------------------------------------------------------------------
-                materialCount = 0
-                materialNames = ''
-                
-                if bpy.context.scene.collectMaterials == True:
-                    
-                    for objSlots in obj.material_slots:
-                        materialSlots = objSlots.material.name.lstrip('"').rstrip('"').replace("'", "").lstrip("Material'")
-                        materialString = "Materials({})=Material'{}'\n\t\t\t\t\t".format(materialCount, materialSlots)
-                        materialCount += 1
-                        materialNames += materialString
-                
-                # LAYER
-                # --------------------------------------------------------------------
-                
-                layerString = str(obj.users_collection[0].name.rstrip(".0123456789"))
-                
-                # TAG
-                # --------------------------------------------------------------------
-                tagString = str(objName[0]).rstrip(".0123456789")
-                
-                # PHYSICAL MATERIAL APPLIED
-                # --------------------------------------------------------------------
-                if bpy.context.scene.physMat == True:
-                    physString = 'PhysicalMaterials.Collision_Sticky'
-                else:
-                    physString = ''
-                    
-                # FORMATTING FOR STATICMESH 
-                # --------------------------------------------------------------------
-                if ('StaticMesh' in str(objName)):                
-                    textUDK += staticMeshString.format(num, staticString, materialNames.rstrip(), physString, locList, rotList, scaleList, tagString, layerString)
-                
-                # FORMATTING FOR SPOTLIGHT 
-                # --------------------------------------------------------------------
-                elif ('Spot' in str(objName)):
-                
-                    # ROTATION XYZ FOR SPOTLIGHTS IS NEGATIVE 90 DEGREES IN UDK SO WE HAVE TO ADJUST IT.
-                    bpy.ops.custom.set_pos_y()
-
-                    rotList = self.getRot(obj.rotation_euler)
-                    
-                    # ADUJUST ROTATION BACK 90 DEGREES
-                    bpy.ops.custom.set_neg_y()
-                    
-                    textUDK += spotLightString.format(num, locList, rotList, scaleList, tagString, layerString)
-                    
-                # FORMATTING FOR CUSTOM GOAL 
-                # --------------------------------------------------------------------
-                elif ('GoalVolume' in str(objName)):
-                    teamNum = 0
-                    
-                    bpy.ops.custom.send_to_t3d()
-
-                    #IF LOCATION == NEGATIVE Y SET TO TEAM NUMBER 1
-                    if locList[1] < 0:
-                        teamNum = 1
-
-                    textUDK += goalString.format(num, teamNum, bpy.context.scene.textT3d, locList, rotList, scaleList, layerString)
-                    
-                elif ('DynamicTrigger' in str(objName)):
-                        
-                    bpy.ops.custom.send_to_t3d()
-
-                    textUDK += dynamicTriggerString.format(num, bpy.context.scene.textT3d, locList, rotList, scaleList, tagString, layerString)
-
-                # FORMATTTING FOR EVERYTHING ELSE 
-                # --------------------------------------------------------------------
-                else:
-                    textUDK += staticMeshString.format(num, staticString, materialNames.rstrip(), physString, locList, rotList, scaleList, tagString, layerString)
+            # FORMATTING FOR STATICMESH 
+            # --------------------------------------------------------------------
+            if ('StaticMesh' in str(objName)):
+                textUDK += staticMeshString.format(num, staticString, materialNames.rstrip(), physString, locList, rotList, scaleList, tagString, layerString, archetype)
             
-                loopCount += 1
-                
-                bpy.context.scene.numberSequencer += 1
+            # FORMATTING FOR SPOTLIGHT 
+            # --------------------------------------------------------------------
+            elif ('Spot' in str(objName)):
+            
+                # ROTATION XYZ FOR SPOTLIGHTS IS NEGATIVE 90 DEGREES IN UDK SO WE HAVE TO ADJUST IT.
+                bpy.ops.custom.set_pos_y()
 
-            f = open( outputFile, 'w' )
-            f.writelines( textUDK.rstrip() )
-            f.close()   
-            pyperclip.copy(textUDK.rstrip())
+                rotList = self.getRot(obj.rotation_euler)
+                
+                # ADUJUST ROTATION BACK 90 DEGREES
+                bpy.ops.custom.set_neg_y()
+                
+                textUDK += spotLightString.format(num, locList, rotList, scaleList, tagString, layerString)
+                
+            # FORMATTING FOR CUSTOM GOAL 
+            # AS FAR AS I CAN TELL YOU CAN'T DO AN ARCHETYPE FOR THIS
+            # --------------------------------------------------------------------
+            elif ('GoalVolume' in str(objName)):
+                
+                teamNum = 0
+                
+                bpy.ops.custom.send_to_t3d()
+
+                #IF LOCATION == NEGATIVE Y SET TO TEAM NUMBER 1
+                if locList[1] < 0:
+                    teamNum = 1
+
+                textUDK += goalString.format(num, teamNum, bpy.context.scene.textT3d, locList, rotList, scaleList, layerString)
+                
+            # FORMATTING FOR DYNAMIC TRIGGER 
+            # AS FAR AS I CAN TELL YOU CAN'T DO AN ARCHETYPE FOR THIS 
+            # --------------------------------------------------------------------                
+            elif ('DynamicTrigger' in str(objName)):
+                
+                    
+                bpy.ops.custom.send_to_t3d()
+
+                textUDK += dynamicTriggerString.format(num, bpy.context.scene.textT3d, locList, rotList, scaleList, tagString, layerString)
+                
+            # FORMATTING FOR CUSTOM BOOST
+            # --------------------------------------------------------------------
+            elif ('Boost' in str(objName)):
+                
+                if bpy.context.scene.customBoostMesh == True:
+                    textUDK += staticMeshString.format(num, staticString, materialNames.rstrip(), physString, locList, rotList, scaleList, tagString, layerString, archetype)
+                elif ("Large" in str(objName)):
+                    textUDK += staticMeshString.format(num, boostLgMesh, materialNames.rstrip(), physString, locList, rotList, scaleList, tagString, layerString, archetype)
+                else:
+                    textUDK += staticMeshString.format(num, boostSmMesh, materialNames.rstrip(), physString, locList, rotList, scaleList, tagString, layerString, archetype)
+              
+                fxLoc = [0.0, 0.0, 0.05]
+                self.setBoostLoc(obj, fxLoc)    
+                fxList = self.getLoc(obj.location)
+                
+                pickupLoc = [0.0, 0.0, 0.67]
+                self.setBoostLoc(obj, pickupLoc)
+                pickupList = self.getLoc(obj.location)
+                
+                if ('Large' in str(objName)):
+                    boostSize = 'Pill'
+                    boostAmount = 9999.000000
+                    boostDelay = 10.0
+                    CollisionRadius = 96.0                            
+                    
+                else:
+                    boostSize = 'Pad'
+                    boostAmount = 0.299900
+                    boostDelay = 4.0
+                    CollisionRadius = 160.0 
+                
+                if bpy.context.scene.customBoostParticles == True:
+                    textUDK += customBoostFxString.format(num, tagString + '_Particles', fxList, rotList, boostSize)
+                    textUDK += customPickUpString.format(num, pickupList, rotList, boostAmount, boostSize, boostDelay, CollisionRadius)
+                    
+                else:
+                    textUDK += boostFxString.format(num, rotList, fxList, boostSize)
+                    textUDK += boostPickupString.format(num, boostSize, pickupList, rotList, boostSize)
+                
+                orgLoc = [0.0, 0.0, -0.72]
+                self.setBoostLoc(obj, orgLoc)
+
+            # SKIP PARTICLES MESHES
+            # --------------------------------------------------------------------
+            elif ('Particles' in str(objName)):
+                textUDK += ''
+                
+            # FORMATTTING FOR EVERYTHING ELSE 
+            # --------------------------------------------------------------------
+            else:
+                textUDK += staticMeshString.format(num, staticString, materialNames.rstrip(), physString, locList, rotList, scaleList, tagString, layerString, archetype)
         
-            return {'FINISHED'}
+            loopCount += 1
+            
+            bpy.context.scene.numberSequencer += 1
+        
+        # if bpy.context.scene.collectRotations == False:
+            # loopCount = 0
+            # while loopCount < objCount:
+                # objHolder[loopCount].select_set(state=True)
+                # loopCount += 1        
+
+        f = open( outputFile, 'w' )
+        f.writelines( textUDK.rstrip() )
+        f.close()   
+        pyperclip.copy(textUDK.rstrip())
+        
+        return {'FINISHED'}
         
     def getRot(self, inVar):
         tempList = []
@@ -213,3 +271,23 @@ class sendToUDK(bpy.types.Operator):
 
         outVar = [tempList[1],tempList[2],tempList[0]]
         return outVar
+        
+    def getLoc(self, inVar):
+        tempList = []
+        locCount = 0
+    
+        for idxLoc in inVar:   
+            if locCount == 0:
+                idxLoc = -idxLoc
+            tempList.append(round(idxLoc * 100, 6))
+            locCount += 1
+            
+        outVar = [tempList[0],tempList[1],tempList[2]]
+        return outVar
+
+    def setBoostLoc(self, inVar1, inVar2):
+    
+        loc = Matrix.Translation((inVar2[0], inVar2[1], inVar2[2]))
+        inVar1.matrix_basis @= loc
+        
+        return
